0f0d550bbb151632bc6ae0a8829bd64068c5fe95
an288705 <aj0466012@gmail.com>
2022-02-28 21:08:34 -0500

    explanation

--- p1.java
+++ p1.java
@@ -7,15 +7,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-// Solution: if a prisoner has never turned on the switch, he turns it on, if the switch was off.
-// If he does turn it on, he remembers he has turned on the switch and never turns it on again.
-// The leader sometimes gets selected, when he is selected, he checks the switch, if it is on then he
-// counts the times the switch was on, if the number of times the switch was turned on equals the
-// number of prisoners then he knows for sure that everyone has visited the switch room and they are
-// free. Because the leader does not know the state of the switch when they start, he waits until he
-// has visited the room and set the switch to off to begin counting and counts hiimself as having
-// entered the room.
-
 //It is known that there is already a birthday cupcake 
 //left at labyrinth’s exit at the start of the game.
 //If a guest has never eaten a cupcake, he eats the
@@ -28,14 +19,14 @@ import java.io.IOException;
 class p1{
     public static AtomicBoolean occupied = new AtomicBoolean(false);
     public static AtomicBoolean cupcake = new AtomicBoolean(true);
-    public static ArrayList<Boolean> available = new ArrayList<Boolean>();
-    public static ArrayList<AtomicBoolean> eaten = new ArrayList<AtomicBoolean>();
+    public static ArrayList<Boolean> current = new ArrayList<Boolean>();
     public static AtomicInteger count= new AtomicInteger(0);
     public static AtomicInteger idx= new AtomicInteger(0);
     public static void main(String[] args)
     {
         ArrayList<Thread> threadList = new ArrayList<Thread>();
         int N=8;
+        long start = System.nanoTime();
 
         Runnable leader = ()->{
             //we know its 0, no need to init
@@ -43,7 +34,7 @@ class p1{
             {
                 //getAndSet returns the previous value, which means that if prev was false,
                 //it immediately becomes true. if prev was true, setting it to true again wont matter
-                if(available.get(0) && !occupied.getAndSet(true))
+                if(current.get(0) && !occupied.getAndSet(true))
                 {
                     //System.out.println("leader in room");
                     if(!cupcake.get())
@@ -55,7 +46,7 @@ class p1{
                     }
                     //now leave room
                     occupied.getAndSet(false);
-                    available.set(0, false);
+                    current.set(0, false);
                 }      
                 //System.out.println("leader waiting");       
             }         
@@ -63,40 +54,40 @@ class p1{
 
         //set leader idx to 0
         threadList.add(new Thread(leader));
-        available.add(false);
-        eaten.add(new AtomicBoolean(true));
+        current.add(false);
         threadList.get(0).start();
 
         Runnable guest = ()->{
             //thread can only access arr ele in its thread idx
-            //if not eaten and cupcake is available, 
+            //if not eaten and cupcake is current, 
             //eat cupcake and set eaten to true
             int threadIdx=idx.incrementAndGet();
+            boolean eaten = false;
             //System.out.println("thread "+threadIdx+" started");
 
             while(count.get()!=N-1)
             {
                 //getAndSet returns the previous value, which means that if prev was false,
                 //it immediately becomes true. if prev was true, setting it to true again wont matter
-                if(available.get(threadIdx) && !occupied.getAndSet(true)) 
+                if(current.get(threadIdx) && !occupied.getAndSet(true)) 
                 {
-                    //available guarantees that the thread can get in the room, 
+                    //current guarantees that the thread can get in the room, 
                     //occupied means that the thread is the ONLY thread in the room
                     //System.out.println("thread "+threadIdx+" occupying room");
                     //System.out.println(occupied);
-                    if(!eaten.get(threadIdx).get() && cupcake.get())
+                    if(!eaten && cupcake.get())
                     {
                         cupcake.getAndSet(false);
-                        eaten.get(threadIdx).getAndSet(true);
+                        eaten=true;
                         System.out.println("thread "+threadIdx+" has eaten cupcake");
                         // System.out.println(eaten);
                         // System.out.println(cupcake);
                     }
                     //now leave room
                     occupied.getAndSet(false);
-                    available.set(threadIdx, false);
+                    current.set(threadIdx, false);
                     //System.out.println("thread "+threadIdx+" left room");
-                    //System.out.println(available);
+                    //System.out.println(current);
                 }
                 //System.out.println("thread "+threadIdx+" waiting");
             }
@@ -105,8 +96,7 @@ class p1{
         for(int i=1;i<N;i++)
         {
             threadList.add(new Thread(guest));
-            available.add(false);
-            eaten.add(new AtomicBoolean(false));
+            current.add(false);
             threadList.get(i).start();          
         }
 
@@ -116,20 +106,26 @@ class p1{
             //since only threads turn off availability,
             //there is no need to worry about threads not
             //making availability in time
+
+            //current problem is that the random function 
+            //needs to pick numbers that haven't been used
             Random random = new Random();
             int rand=random.nextInt(N);
-            available.set(rand, true);     
+            current.set(rand, true);     
         }
+
+        long end = System.nanoTime();
 
         if(count.get()==N-1)
         {
             System.out.println("All guests have visited");
+            System.out.println("The guests took "+String.valueOf((end-start)/1000000000)+" seconds to eat the cupcakes");
         }
     }
 }
 
 //choose idx then open occupy for idx
-//problem with atomic available is that it would HAVE to be next thread
+//problem with atomic current is that it would HAVE to be next thread
 //current problem is that the leader might be putting cupcakes for himseld and not remembering
 //basically, the first eaten cupcake is read correctly, but as the leader is in the process of putting a new cupcake, 
 //a new prisoner eats the cupcake. this is a collision that causes missed counts
